060a802c3bb48219e7dad98960df9fcc544f92be
takashi145 <t.shuhei0203@gmail.com>
2023-03-02 02:21:42 +0900

    edit memo list page

--- src/pages/MemoList.jsx
+++ src/pages/MemoList.jsx
@@ -19,18 +19,21 @@ const About = () => {
   return (
     <div className='m-8 max-w-7xl '>
       <h3 className='text-xl mb-3'>メモ一覧</h3>
-      <div className='flex flex-col md:flex-row  md:space-x-8'>
+      <div className='flex flex-wrap'>
         {
             memos.map((memo) => {
               return (
-                <Link 
-                  to={`/memo/${memo.id}`} 
-                  key={memo.id} 
-                  class="hover:ring-2 my-3 mx-auto w-full md:w-1/3 block p-3 bg-gray-100 border border-gray-200 rounded-lg shadow hover:bg-gray-100"
-                >
-                    <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">{ memo.title }</h5>
-                    <p class="font-normal text-sm text-gray-700 text-end">{ TimeDiff(memo.updated_at) }</p>
-                </Link> 
+                <div className='w-full sm:w-1/2 lg:w-1/3'>
+                  <Link 
+                    to={`/memo/${memo.id}`} 
+                    key={memo.id} 
+                    class="hover:ring-2 m-3 block p-3 bg-gray-100 border border-gray-200 rounded-lg shadow hover:bg-gray-100"
+                  >
+                      <h5 class="mb-2 text-lg font-bold tracking-tight text-gray-900">{ memo.title }</h5>
+                      <p class="font-normal text-sm text-gray-700 text-end">{ TimeDiff(memo.updated_at) }</p>
+                  </Link>   
+                </div>
+                
               )
             })
           }
